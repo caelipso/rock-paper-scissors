c8791cd4b5ed47c66b428b3016ad0862bcfe49d2
caelipso <92943384+caelipso@users.noreply.github.com>
2022-02-17 23:20:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
-//box user choice
-let playerSelection = prompt("Do you choose rock, paper or scissors?");
+userChoice = prompt("rock paper or scissors?");
+console.log(userChoice);
 
-//randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-let computerSelection = Math.random();
-let results = function computerResult() {
-  if (computerSelection < 0.34) {
-    return "rock";
-  } else if (computerSelection <= 0.67) {
+randomNumber = Math.random();
+console.log(randomNumber);
+
+function conversionToGame(randomNumber){
+  if(randomNumber<0.4){
     return "paper";
-  } else {
-    return "scissors";
-  }
-  console.log(computerResult());
-};
-
-//compare results between user and computer
-function compare(playerSelection, results) {
-  if (playerSelection === results) {
-    return "The result is a tie!";
-  } else if (playerSelection === "rock") {
-    if (results === "scissors") {
-      return "You won rock beats scissors";
-    } else if (results === "paper") {
-      return "You lose paper beats rock";
-    }
-  } else if (playerSelection === "paper") {
-    if (results === "scissors") {
-      return "You lose scissors beats paper";
-    } else if (results === "rock") {
-      return "You won paper beats rock";
-    }
-  } else if (playerSelection === "scissors") {
-    if (results === "paper") {
-      return "You won scissors beats paper";
-    } else if (results === "rock") {
-      return "You lose rock beats scissors";
-    }
-  }
+  }if else (randomNumber>=0.4)
 }
